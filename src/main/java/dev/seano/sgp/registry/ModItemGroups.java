0549bb308178853df82d5424ebed4b812231a7c2
Sean O'Connor <so@seano.dev>
2023-09-11 09:36:32 -0600

    Update SGP_TAB icon

--- src/main/java/dev/seano/sgp/registry/ModItemGroups.java
+++ src/main/java/dev/seano/sgp/registry/ModItemGroups.java
@@ -7,6 +7,8 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
+import net.minecraft.world.item.Items;
+
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -17,7 +19,7 @@ public class ModItemGroups {
 			Registries.CREATIVE_MODE_TAB, SGP.MOD_ID);
 
 	public static final RegistryObject<CreativeModeTab> SGP_TAB = ITEM_GROUPS.register(SGP.MOD_ID,
-			() -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.EXAMPLE_ITEM.get()))
+			() -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.WHEAT))
 					.title(Component.translatable("itemGroup." + SGP.MOD_ID)).displayItems(
 							(p_270258_, p_259752_) -> ModItems.ITEMS.getEntries()
 									.forEach(itemRegistryObject -> p_259752_.accept(itemRegistryObject.get())))
