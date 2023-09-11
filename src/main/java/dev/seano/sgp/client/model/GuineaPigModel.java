9eb33e49dc3a05d610da14d2b3c02b275a0ad4c6
Sean O'Connor <so@seano.dev>
2023-09-11 12:13:33 -0600

    Update Guinea Pig entity and model
    Add food items, scale baby guinea pig

--- src/main/java/dev/seano/sgp/client/model/GuineaPigModel.java
+++ src/main/java/dev/seano/sgp/client/model/GuineaPigModel.java
@@ -1,14 +1,12 @@
 package dev.seano.sgp.client.model;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-
+import com.google.common.collect.ImmutableList;
 import dev.seano.sgp.SGP;
 import dev.seano.sgp.entity.GuineaPig;
 
+import net.minecraft.client.model.AgeableListModel;
 import org.jetbrains.annotations.NotNull;
 
-import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
@@ -23,86 +21,85 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class GuineaPigModel<T extends Entity> extends EntityModel<T> {
+public class GuineaPigModel<T extends Entity> extends AgeableListModel<T> {
 
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(SGP.resourceLocation(GuineaPig.ID),
-			"main");
+    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(SGP.resourceLocation(GuineaPig.ID),
+            "main");
 
-	private final ModelPart body;
-	private final ModelPart rightHindLeg;
-	private final ModelPart leftHindLeg;
-	private final ModelPart rightFrontLeg;
-	private final ModelPart leftFrontLeg;
-	private final ModelPart head;
+    private final ModelPart body;
+    private final ModelPart rightHindLeg;
+    private final ModelPart leftHindLeg;
+    private final ModelPart rightFrontLeg;
+    private final ModelPart leftFrontLeg;
+    private final ModelPart head;
 
-	public GuineaPigModel(ModelPart root) {
-		this.body = root.getChild("body");
-		this.rightHindLeg = root.getChild("rightHindLeg");
-		this.leftHindLeg = root.getChild("leftHindLeg");
-		this.rightFrontLeg = root.getChild("rightFrontLeg");
-		this.leftFrontLeg = root.getChild("leftFrontLeg");
-		this.head = root.getChild("head");
-	}
+    public GuineaPigModel(ModelPart root) {
+        super(true, 8f, 0f);
+        this.body = root.getChild("body");
+        this.rightHindLeg = root.getChild("rightHindLeg");
+        this.leftHindLeg = root.getChild("leftHindLeg");
+        this.rightFrontLeg = root.getChild("rightFrontLeg");
+        this.leftFrontLeg = root.getChild("leftFrontLeg");
+        this.head = root.getChild("head");
+    }
 
-	@SuppressWarnings("unused")
-	public static LayerDefinition createBodyLayer() {
-		MeshDefinition meshdefinition = new MeshDefinition();
-		PartDefinition partdefinition = meshdefinition.getRoot();
+    @SuppressWarnings("unused")
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0)
-						.addBox(-1.5F, -2.75F, -2.0F, 3.0F, 3.0F, 6.0F, new CubeDeformation(0.0F)).texOffs(12, 2)
-						.addBox(-0.5F, -1.25F, 3.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 23.5F, 0.0F));
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0)
+                        .addBox(-1.5F, -2.75F, -2.0F, 3.0F, 3.0F, 6.0F, new CubeDeformation(0.0F)).texOffs(12, 2)
+                        .addBox(-0.5F, -1.25F, 3.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, 23.5F, 0.0F));
 
-		PartDefinition rightHindLeg = partdefinition.addOrReplaceChild("rightHindLeg",
-				CubeListBuilder.create().texOffs(0, 0)
-						.addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(-0.75F, 23.5F, 3.25F));
+        PartDefinition rightHindLeg = partdefinition.addOrReplaceChild("rightHindLeg",
+                CubeListBuilder.create().texOffs(0, 0)
+                        .addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(-0.75F, 23.5F, 3.25F));
 
-		PartDefinition leftHindLeg = partdefinition.addOrReplaceChild("leftHindLeg",
-				CubeListBuilder.create().texOffs(0, 2)
-						.addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.75F, 23.5F, 3.25F));
+        PartDefinition leftHindLeg = partdefinition.addOrReplaceChild("leftHindLeg",
+                CubeListBuilder.create().texOffs(0, 2)
+                        .addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.75F, 23.5F, 3.25F));
 
-		PartDefinition rightFrontLeg = partdefinition.addOrReplaceChild("rightFrontLeg",
-				CubeListBuilder.create().texOffs(0, 4)
-						.addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(-0.75F, 23.5F, -3.25F));
+        PartDefinition rightFrontLeg = partdefinition.addOrReplaceChild("rightFrontLeg",
+                CubeListBuilder.create().texOffs(0, 4)
+                        .addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(-0.75F, 23.5F, -3.25F));
 
-		PartDefinition leftFrontLeg = partdefinition.addOrReplaceChild("leftFrontLeg",
-				CubeListBuilder.create().texOffs(12, 0)
-						.addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.75F, 23.5F, -3.25F));
+        PartDefinition leftFrontLeg = partdefinition.addOrReplaceChild("leftFrontLeg",
+                CubeListBuilder.create().texOffs(12, 0)
+                        .addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.75F, 23.5F, -3.25F));
 
-		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 9)
-						.addBox(-1.5F, -1.375F, -3.0F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(9, 9)
-						.addBox(-1.5F, -0.375F, -4.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(3, 2)
-						.addBox(-2.0F, -0.875F, -1.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)).texOffs(3, 0)
-						.addBox(1.0F, -0.875F, -1.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 22.125F, -1.0F));
+        PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 9)
+                        .addBox(-1.5F, -1.375F, -3.0F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(9, 9)
+                        .addBox(-1.5F, -0.375F, -4.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).texOffs(3, 2)
+                        .addBox(-2.0F, -0.875F, -1.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)).texOffs(3, 0)
+                        .addBox(1.0F, -0.875F, -1.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, 22.125F, -1.0F));
 
-		return LayerDefinition.create(meshdefinition, 32, 32);
-	}
+        return LayerDefinition.create(meshdefinition, 32, 32);
+    }
 
-	@Override
-	public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
-						  float headPitch) {
-//		this.head.xRot = headPitch * ((float) Math.PI / 180F);
-//		this.head.yRot = netHeadYaw * ((float) Math.PI / 180F);
-//		this.rightHindLeg.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-//		this.leftHindLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
-//		this.rightFrontLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
-//		this.leftFrontLeg.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-	}
+    @Override
+    protected @NotNull Iterable<ModelPart> headParts() {
+        return ImmutableList.of(this.head);
+    }
 
-	@Override
-	public void renderToBuffer(@NotNull PoseStack poseStack, @NotNull VertexConsumer vertexConsumer, int packedLight,
-							   int packedOverlay, float red, float green, float blue, float alpha) {
-		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		rightHindLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		leftHindLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		rightFrontLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		leftFrontLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-	}
+    @Override
+    protected @NotNull Iterable<ModelPart> bodyParts() {
+        return ImmutableList.of(this.body, this.rightHindLeg, this.leftHindLeg, this.rightFrontLeg, this.leftFrontLeg);
+    }
+
+    @Override
+    public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+                          float headPitch) {
+        this.head.xRot = headPitch * 0.017453292F;
+        this.head.yRot = netHeadYaw * 0.017453292F;
+        double v = (Math.cos(limbSwing * 1.3324F) * 1.4F * limbSwingAmount) / 6;
+        this.head.zRot = (float) v;
+        this.body.zRot = (float) v;
+    }
 }
--- src/main/java/dev/seano/sgp/entity/GuineaPig.java
+++ src/main/java/dev/seano/sgp/entity/GuineaPig.java
@@ -2,47 +2,56 @@ package dev.seano.sgp.entity;
 
 import dev.seano.sgp.registry.ModEntityTypes;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.BreedGoal;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.PanicGoal;
-import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class GuineaPig extends Animal {
 
-	public static final String ID = "guinea_pig";
+    public static final String ID = "guinea_pig";
 
-	public GuineaPig(EntityType<? extends Animal> pEntityType, Level pLevel) {
-		super(pEntityType, pLevel);
-	}
+    private static final Ingredient FOOD_ITEMS = Ingredient.of(Items.WHEAT, Items.APPLE, Items.MELON_SLICE,
+            Items.BEETROOT, Items.CARROT, Blocks.WHEAT, Items.GOLDEN_APPLE);
 
-	public static AttributeSupplier.Builder createGuineaPigAttributes() {
-		return createMobAttributes().add(Attributes.MAX_HEALTH, 4d).add(Attributes.MOVEMENT_SPEED, 0.25d);
-	}
+    public GuineaPig(EntityType<? extends Animal> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
 
-	@Override
-	protected void registerGoals() {
-		this.goalSelector.addGoal(1, new FloatGoal(this));
-		this.goalSelector.addGoal(2, new PanicGoal(this, 1.05f));
-		this.goalSelector.addGoal(3, new BreedGoal(this, 0.95f));
-		this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1f, 1.0000001E-5f));
-		this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 10f));
-	}
+    public static AttributeSupplier.Builder createGuineaPigAttributes() {
+        return createMobAttributes().add(Attributes.MAX_HEALTH, 4d).add(Attributes.MOVEMENT_SPEED, 0.25d);
+    }
 
-	@Nullable
-	@Override
-	public AgeableMob getBreedOffspring(@NotNull ServerLevel pLevel, @NotNull AgeableMob pOtherParent) {
-		return ModEntityTypes.GUINEA_PIG.get().create(pLevel);
-	}
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(1, new FloatGoal(this));
+        this.goalSelector.addGoal(2, new PanicGoal(this, 1.05f));
+        this.goalSelector.addGoal(3, new BreedGoal(this, 0.95f));
+        this.goalSelector.addGoal(4, new TemptGoal(this, 1f, FOOD_ITEMS, false));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1f, 1.0000001E-5f));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 10f));
+    }
+
+    @Override
+    public boolean isFood(@NotNull ItemStack itemStack) {
+        return FOOD_ITEMS.test(itemStack);
+    }
+
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(@NotNull ServerLevel pLevel, @NotNull AgeableMob pOtherParent) {
+        return ModEntityTypes.GUINEA_PIG.get().create(pLevel);
+    }
 }
