91f514eb1c56481a1dd875b3014a413e9c63adac
Sean O'Connor <so@seano.dev>
2023-09-10 22:39:16 -0600

    Initial commit

---
+++ src/main/java/dev/seano/sgp/SGP.java
@@ -0,0 +1,38 @@
+package dev.seano.sgp;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SGP.MOD_ID)
+public class SGP {
+    public static final String MOD_ID = "sgp";
+    private static final Logger LOGGER = LogManager.getLogger(StringUtils.capitalize(MOD_ID));
+
+    public SGP() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("common setup");
+    }
+
+    @Mod.EventBusSubscriber(modid = SGP.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value =
+            Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("client setup");
+        }
+    }
+}
