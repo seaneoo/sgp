50022598af33af837c4e486b6cea0223d1a0bb33
Sean O'Connor <so@seano.dev>
2023-09-11 08:58:54 -0600

    Add .editorconfig
    Reformat source code

--- src/main/java/dev/seano/sgp/SGP.java
+++ src/main/java/dev/seano/sgp/SGP.java
@@ -1,6 +1,10 @@
 package dev.seano.sgp;
 
 import dev.seano.sgp.registry.ModItems;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -9,34 +13,30 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(SGP.MOD_ID)
 public class SGP {
-    public static final String MOD_ID = "sgp";
-    private static final Logger LOGGER = LogManager.getLogger(StringUtils.capitalize(MOD_ID));
+	public static final String MOD_ID = "sgp";
+	private static final Logger LOGGER = LogManager.getLogger(StringUtils.capitalize(MOD_ID));
 
-    public SGP() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::commonSetup);
+	public SGP() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::commonSetup);
 
-        ModItems.init(modEventBus);
+		ModItems.init(modEventBus);
 
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("common setup");
-    }
+	private void commonSetup(final FMLCommonSetupEvent event) {
+		LOGGER.info("common setup");
+	}
 
-    @Mod.EventBusSubscriber(modid = SGP.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value =
-            Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            LOGGER.info("client setup");
-        }
-    }
+	@Mod.EventBusSubscriber(modid = SGP.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents {
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event) {
+			LOGGER.info("client setup");
+		}
+	}
 }
--- src/main/java/dev/seano/sgp/registry/ModItems.java
+++ src/main/java/dev/seano/sgp/registry/ModItems.java
@@ -1,20 +1,21 @@
 package dev.seano.sgp.registry;
 
 import dev.seano.sgp.SGP;
+
 import net.minecraft.world.item.Item;
+
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, SGP.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SGP.MOD_ID);
 
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
-            () -> new Item(new Item.Properties()));
+	public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+			() -> new Item(new Item.Properties()));
 
-    public static void init(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+	public static void init(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
 }
