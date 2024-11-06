b9a43dbc7435133e4190750bed16c247ac58e4a0
Sean O'Connor <so@seano.dev>
2024-11-05 21:56:47 -0700

    Add placeholder sounds
    This will be changed to custom sounds later on.

--- src/main/kotlin/dev/seano/sgp/entity/GuineaPigEntity.kt
+++ src/main/kotlin/dev/seano/sgp/entity/GuineaPigEntity.kt
@@ -2,16 +2,21 @@ package dev.seano.sgp.entity
 
 import dev.seano.sgp.registry.SGPEntities
 import dev.seano.sgp.registry.SGPTags.GUINEA_PIG_FOOD
+import net.minecraft.block.BlockState
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.ai.goal.*
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.mob.MobEntity
 import net.minecraft.entity.passive.AnimalEntity
 import net.minecraft.entity.passive.PassiveEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 class GuineaPigEntity(entityType: EntityType<out AnimalEntity>?, world: World?) : AnimalEntity(entityType, world) {
@@ -37,6 +42,26 @@ class GuineaPigEntity(entityType: EntityType<out AnimalEntity>?, world: World?) 
 		goalSelector.add(6, LookAtEntityGoal(this, PlayerEntity::class.java, 6.0f))
 		goalSelector.add(7, LookAtEntityGoal(this, GuineaPigEntity::class.java, 4.0f))
 		goalSelector.add(8, LookAroundGoal(this))
+	}
+
+	override fun getAmbientSound(): SoundEvent? {
+		// TODO 2024-11-5, 21:56 custom sound
+		return SoundEvents.ENTITY_PIG_AMBIENT
+	}
+
+	override fun getHurtSound(source: DamageSource?): SoundEvent? {
+		// TODO 2024-11-5, 21:56 custom sound
+		return SoundEvents.ENTITY_PIG_HURT
+	}
+
+	override fun getDeathSound(): SoundEvent? {
+		// TODO 2024-11-5, 21:56 custom sound
+		return SoundEvents.ENTITY_PIG_DEATH
+	}
+
+	override fun playStepSound(pos: BlockPos?, state: BlockState?) {
+		// TODO 2024-11-5, 21:56 custom sound
+		this.playSound(SoundEvents.ENTITY_PIG_STEP, 0.15f, 1.0f)
 	}
 
 	override fun createChild(world: ServerWorld?, entity: PassiveEntity?): PassiveEntity? {
