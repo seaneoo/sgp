6ad2dc579406c23e8bba3998f3200ca127bee1f7
Sean O'Connor <so@seano.dev>
2024-11-07 15:53:53 -0700

    Scale baby Guinea Pig model

--- src/client/kotlin/dev/seano/sgp/GuineaPigEntityRenderer.kt
+++ src/client/kotlin/dev/seano/sgp/GuineaPigEntityRenderer.kt
@@ -4,9 +4,11 @@ import dev.seano.sgp.entity.GuineaPigEntity
 import dev.seano.sgp.registry.SGPEntities
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.minecraft.client.render.VertexConsumerProvider
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.render.entity.MobEntityRenderer
 import net.minecraft.client.render.entity.model.EntityModelLayer
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
 
 @Environment(EnvType.CLIENT)
@@ -22,5 +24,19 @@ class GuineaPigEntityRenderer(context: EntityRendererFactory.Context) :
 
 	override fun getTexture(entity: GuineaPigEntity?): Identifier {
 		return SGP.id("textures/entity/guinea_pig.png")
+	}
+
+	override fun render(
+		livingEntity: GuineaPigEntity,
+		f: Float,
+		g: Float,
+		matrixStack: MatrixStack,
+		vertexConsumerProvider: VertexConsumerProvider,
+		i: Int
+	) {
+		if (livingEntity.isBaby) {
+			matrixStack.scale(0.75f, 0.75f, 0.75f)
+		}
+		super.render(livingEntity, f, g, matrixStack, vertexConsumerProvider, i)
 	}
 }
